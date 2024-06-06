7a127b7c5fd8f76eca1a95093214f9fc7f932506
sahubabu <ashok.pydev@gmail.com>
2024-06-06 23:40:05 +0530

    added setup and requirements file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project Ques_maker",
+    version= '0.0.0',
+    author="Ashok Kumar Sahu",
+    author_email="ashok.pydevgmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
